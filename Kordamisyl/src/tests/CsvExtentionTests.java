d4a1c9f33753079713974940acf5da07dfcf5f37
kristenspirka12 <krispi98@gmail.com>
2018-11-06 14:18:22 +0200

    Add files via upload

--- Kordamisyl/src/tests/CsvExtentionTests.java
+++ Kordamisyl/src/tests/CsvExtentionTests.java
@@ -1,5 +1,19 @@
 package tests;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Test;
+
 public class CsvExtentionTests {
 
+	String[] csvArray = csvToArray("tool, laud, lamp, arvuti");
+	
+	/*@Test
+	public void CsvToListCorrectValue() {
+		assertThat(csvArray, is(csvToArray));
+	}*/
+	
+	public static String[] csvToArray(String csvStr) {
+		return csvStr.split("\\s*(,)\\s*");
+	}
 }
