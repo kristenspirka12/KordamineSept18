beb98110b5e271e506108757b7e79feb78d632c3
kristenspirka12 <krispi98@gmail.com>
2018-11-06 10:16:30 +0200

    Add files via upload

--- Kordamisyl/src/tests/HeadphoneTests.java
+++ Kordamisyl/src/tests/HeadphoneTests.java
@@ -21,7 +21,7 @@ public class HeadphoneTests {
 	public void baseLoweredPriceCalculation() {
 		
 		/*Headphone headphoneOdavad = new Headphone("Philips", 5.4, false);
-		Double arvutatudHind = headphoneOdavad.get10LowerPrice();
+		Double arvutatudHind = headphoneOdavad.getLowerPrice();
 		Double oodatudHind = 4.86;
 		
 		assertThat(arvutatudHind, is(oodatudHind)); */
@@ -34,9 +34,29 @@ public class HeadphoneTests {
 		assertThat(headphoneOdavad.getLowerPrice(30), is(3.78));
 	}
 	
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void loweredPriceCalculationOver100Percent() {
-		headphoneOdavad.getLowerPrice(110);
+		headphoneOdavad.getLowerPrice(105);
 	}
 	
+	@Test(expected = IllegalArgumentException.class)
+	public void loweredPriceCalculationNegativeNumber() {
+		Headphone headphone3 = new Headphone("Samsung",60 ,false);
+		headphone3.getLowerPrice(-15);
+	}
+	
+	@Test
+	public void priceSetterSetValue() {	
+		Headphone headphone1 = new Headphone("Sony", false);
+		headphone1.setPrice(2.0);
+		
+		assertThat(headphone1.getPrice(), is(2.0));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void priceSetterNotSetValueWhenNegative() {	
+		Headphone headphone1 = new Headphone("Sony", false);
+		headphone1.setPrice(-2.0);
+	}
 }
+
