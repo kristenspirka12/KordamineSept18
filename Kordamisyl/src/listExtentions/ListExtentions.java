14bde0941b91ae0fe6441a420439e09ff173e5c2
kristenspirka12 <krispi98@gmail.com>
2018-11-06 15:14:34 +0200

    Add files via upload

--- Kordamisyl/src/listExtentions/ListExtentions.java
+++ Kordamisyl/src/listExtentions/ListExtentions.java
@@ -7,7 +7,13 @@ public class ListExtentions {
 
 	//a) esimese elementide järel teised. [a, b], [c, d] -> [a, b, c, d]
 	public static List<String> addList(List<String> listA, List<String> listB) {
-		return new ArrayList<>();
-		
+		List<String> result = new ArrayList<>(listA);
+		result.addAll(listB);
+		return result;	
+	}
+	
+	//b) teise listi elementide järel esimesed. [a, b], [c, d] -> [c, d, a, b]
+	public static List<String> addListBeginning(List<String> listA, List<String> listB) {
+		return addList(listB, listA);	
 	}
 }
