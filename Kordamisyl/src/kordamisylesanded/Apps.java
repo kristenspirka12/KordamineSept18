bfc11201b762841b4ec9b34acc0003e774c2f916
kristenspirka12 <krispi98@gmail.com>
2018-11-06 10:14:26 +0200

    Add files via upload

--- Kordamisyl/src/kordamisylesanded/Apps.java
+++ Kordamisyl/src/kordamisylesanded/Apps.java
@@ -66,45 +66,9 @@ public class Apps {
 		//System.out.println(headphone1.get10LowerPrice());
 		//System.out.println(headphone1.getLowerPrice(25));
 		
-		/*String[] csvArray = csvToArray("tool=, laud, lamp, arvuti");
-		//System.out.println(Arrays.toString(csvArray));
-		//System.out.println(csvArray[1]);
-		 
 
-		/*List<String> csvList = csvToList("maja, auto, koduloom, arvuti, Juku, elamu");
-		// System.out.println(csvList);
-		// System.out.println(csvList.get(1));
 		//System.out.println(startsWithA(csvList));
-		System.out.println(isVowel('P'));*/
-	}
-
-	public static String[] csvToArray(String csvStr) {
-		return csvStr.split("\\s*(,)\\s*");
-	}
-
-	public static List<String> csvToList(String csvStr) {
-		/* Variant 1
-		List<String> result = new ArrayList<>();
-		for (String item : csvStr.split(",")) {
-		 		result.add(item).trim());
-		} 
-		return result; */
-
-		// Variant 2
-		/* List<String> result = new ArrayList<>();
-		Arrays.asList(csvStr.split(",")).forEach(item -> { 
-		 		result.add(item.trim());
-		});
-		return result; */
-		 
-
-		List<String> result = new ArrayList<>();
-		StringTokenizer st = new StringTokenizer(csvStr, ",");
-		while (st.hasMoreTokens()) {
-			String token = st.nextToken();
-			result.add(token.trim());
-		}
-		return result;
+		//System.out.println(isVowel('P'));
 	}
 
 	public static List<String> startsWithA(List<String> list) {
