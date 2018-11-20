9177879d6df329afdf8c979e1071e13ea67d51c5
kristenspirka12 <krispi98@gmail.com>
2018-11-20 09:21:09 +0200

    Add files via upload

--- Kordamisyl/src/listExtentions/ListExtentions.java
+++ Kordamisyl/src/listExtentions/ListExtentions.java
@@ -5,15 +5,41 @@ import java.util.List;
 
 public class ListExtentions {
 
-	//a) esimese elementide j�rel teised. [a, b], [c, d] -> [a, b, c, d]
+	// a) esimese elementide järel teised. [a, b], [c, d] -> [a, b, c, d]
 	public static List<String> addList(List<String> listA, List<String> listB) {
 		List<String> result = new ArrayList<>(listA);
 		result.addAll(listB);
-		return result;	
+		return result;
 	}
-	
-	//b) teise listi elementide j�rel esimesed. [a, b], [c, d] -> [c, d, a, b]
+
+	// b) teise listi elementide järel esimesed. [a, b], [c, d] -> [c, d, a, b]
 	public static List<String> addListBeginning(List<String> listA, List<String> listB) {
-		return addList(listB, listA);	
+		return addList(listB, listA);
+	}
+
+	// c) vaheldumisi mõlema listi elemendid alates esimese listi elementidest. [a,b], [c, d] -> [a, c, b, d]
+	public static List<String> addListAlternately(List<String> listA, List<String> listB) {
+		List<String> result = new ArrayList<>();
+		int listASize = listA.size();
+		int listBSize = listB.size();
+
+		for (int i = 0; i < Math.max(listASize, listBSize); i++) {
+			if (listASize > i)
+				result.add(listA.get(i));
+			if (listBSize > i)
+				result.add(listB.get(i));
+		}
+		return result;
+
+		// Variant, kui listid peavad olema �he pikkused.
+		/* List<String> result = new ArrayList<>();
+		if (listA.size() != listB.size())
+			throw new IllegalArgumentException("Listid peavad olema ühepikkused.");
+
+		for (int i = 0; i < listA.size(); i++) {
+			result.add(listA.get(i));
+			result.add(listB.get(i));
+		}
+		return result; */
 	}
 }
