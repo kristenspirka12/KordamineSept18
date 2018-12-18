845e0a10a3d88ad1457b1a6f427ece6801485a3f
kristenspirka12 <krispi98@gmail.com>
2018-12-18 11:50:38 +0200

    Add files via upload

--- Kordamisyl/src/mootorsoidukid/Uks.java
+++ Kordamisyl/src/mootorsoidukid/Uks.java
@@ -9,5 +9,25 @@ public class Uks {
 		this.onAvatud = onAvatud;
 		this.ukseliik = ukseliik;
 	}
+	
+	public void sulge() {
+		this.onAvatud = false;
+	}
+	
+	public UkseLiik annaLiik() {
+		return this.ukseliik;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Uks [onAvatud=" + onAvatud + ", ukseliik=" + ukseliik + "]";
+	}
+	
+	public void ava() {
+		
+	}
 }
 	
